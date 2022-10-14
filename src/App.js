b7869d76a2f5601e9f372c30d207a807eb474d7f
Maximo Fermin Pereyra <pereyra.maximo@gmail.com>
2022-10-14 14:01:19 -0300

    cambios en navegation y agrego login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home/Home';
+import Navigation from './routes/Navigation/Navigation';
+import Login from './routes/Login/Login';
 import WeatherCreation from './routes/Weather/WeatherCreation'
 
 
@@ -9,8 +11,13 @@ function App() {
     <div className="App">
       {/* definimos distintos puntos de navegacion para la app */}
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/formulario' element={<WeatherCreation/>}/>
+        <Route path='/' element={<Navigation/>}>
+           <Route index element={<Home/>}/>
+           <Route path='login' element={<Login/>}/>
+           <Route path='/formulario' element={<WeatherCreation/>}/>
+        </Route>
+        
+
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import { FormProvider } from './context/FormContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FormProvider>
-        <App />
-      </FormProvider>
+      <UserProvider>
+        <FormProvider>
+          <App />
+        </FormProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
